845c5dabfb14c530a30940a75cdf79eae6ba7c3b
Illia Borshch <borsch.fo@gmail.com>
2024-04-21 20:31:16 +0100

    Gin setup

--- cmd/webapp/main.go
+++ cmd/webapp/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	handlers "github.com/ilborsch/leetGo-web/internal/handlers/index"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	// init config
+
+	// init logger
+
+	// init gin
+	r := gin.Default()
+
+	// setup routes and middleware
+	setupMiddleware(r)
+	setupRoutes(r)
+
+	// start app
+	if err := r.Run(); err != nil {
+		fmt.Println("fatal error while run")
+		return
+	}
+}
+
+func setupRoutes(r *gin.Engine) {
+	r.GET("/", handlers.Index)
+}
+
+func setupMiddleware(r *gin.Engine) {
 }
